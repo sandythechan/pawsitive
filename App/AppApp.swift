70a68b897a6c5d22602fef3b1671bdc56ba5cdbb
sandythechan <kayuuuuc@gmail.com>
2025-02-21 00:09:09 +0800

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Chan Ka Yu on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by Chan Ka Yu on 21/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
