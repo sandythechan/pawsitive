9b4f81b46e9e7d33e9ff44387dcab621d6d470d6
sandythechan <kayuuuuc@gmail.com>
2025-03-21 10:44:09 +0800

    init

---
+++ App/Views/SleepModeView.swift
@@ -0,0 +1,18 @@
+//
+//  SleepModeView.swift
+//  App
+//
+//  Created by Chan Ka Yu on 21/2/2025.
+//
+
+import SwiftUI
+
+struct SleepModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SleepModeView()
+}
